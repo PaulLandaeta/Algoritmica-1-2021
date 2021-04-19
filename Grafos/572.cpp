66de815156b2cff87fb17e847819312bdf1a4718
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-04-19 09:12:55 -0400

    dfs 572

---
+++ Grafos/572.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+using namespace std; 
+int filas, columnas, nroDepositos;
+vector<string> matriz;
+int di[]={0,1,1,1,0,-1,-1,-1};
+int dj[]={1,1,0,-1,-1,-1,0,1}; 
+void dfs(int fila, int columna) {
+    matriz[fila][columna] = 'A';
+    for(int i = 0; i < 8 ; i++) {
+        int nuevafila = fila + di[i];
+        int nuevacolumna = columna + dj[i];
+        if( nuevafila >= 0 && nuevafila < filas && nuevacolumna >= 0 
+            && nuevacolumna < columnas && 
+            matriz[nuevafila][nuevacolumna] == '@') {
+                dfs(nuevafila,nuevacolumna);
+        }
+
+    }
+}
+
+int main(){ 
+    input;
+    output;
+    while(cin>>filas>>columnas && filas && columnas){
+
+        for(int i = 0;i < filas; i++) {
+            string fila;
+            cin>>fila;
+            matriz.push_back(fila);
+        }
+        nroDepositos = 0;
+        for(int i = 0; i < filas ; i++) {
+            for(int j = 0; j < columnas; j++) {
+                if(matriz[i][j] == '@') {
+                    dfs(i,j);
+                    nroDepositos++;
+                }
+            }
+        }
+        cout<<nroDepositos<<endl;
+        matriz.clear();
+    }
+}

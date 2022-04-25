34ebfba6e4781bb3da0763911674b2b477dd19f9
Paul Landaeta <paul.landeata@coxautoinc.com>
2022-04-25 07:44:07 -0400

    comments into dijkstra algorithm

--- Grafos/dijkstra.cpp
+++ Grafos/dijkstra.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h> // Para importar todas librerias
 #define input freopen("in.txt", "r", stdin)
 #define output freopen("out.txt", "w", stdout)
-#define INF 100000010
+#define INF 100000010 // 1e9 
 using namespace std;
-
+// Pair Dist, vertice // la distancia primero porque necesitamos ordenar de acuerdo a la distancia
 vector<pair<int, int> > grafo[100000];
 int visitados[100000]; 
 int distancia[100000];
@@ -13,19 +13,19 @@ void dijkstra(int verticeInicial)
     // memset(distancia, INF, sizeof(distancia)); // -1 0 1
     for(int i = 0; i < 100000; i++)
         distancia[i] = INF;
-
-
+    // Todos los nodos como no visitado
     memset(visitados, 0, sizeof(visitados));
-
-    multiset<pair<int, int> > colaPrioridad;
+   // cola de prioridad priority_queue <pair<int, int>, greater<> > cola;
+   // va ordenar de mayor a menor 
+    multiset<pair<int, int> > colaPrioridad; 
     distancia[verticeInicial] = 0;
     colaPrioridad.insert(make_pair(0, verticeInicial));
     while (!colaPrioridad.empty())
     {
         pair<int, int> verticeActual = *colaPrioridad.begin();
-        colaPrioridad.erase(colaPrioridad.begin());
-        int vertice = verticeActual.second;
-        int peso = verticeActual.first;
+        colaPrioridad.erase(colaPrioridad.begin()); // pop()
+        int vertice = verticeActual.second; // el nodo de donde se esta viniendo
+        int peso = verticeActual.first; // distancia 
 
         if (visitados[vertice])
         {
@@ -37,9 +37,11 @@ void dijkstra(int verticeInicial)
         {
             int verticeVecino = grafo[vertice][i].second;
             int pesoVecino = grafo[vertice][i].first;
-            if (distancia[vertice] - pesoVecino < distancia[verticeVecino])
+            //    inf  >                    0 + 60
+            // Relajacion 
+            if (distancia[verticeVecino]> distancia[vertice] + pesoVecino )
             {
-                distancia[verticeVecino] = distancia[vertice] - pesoVecino;
+                distancia[verticeVecino] = distancia[vertice] + pesoVecino;
                 colaPrioridad.insert(make_pair(distancia[verticeVecino], verticeVecino));
             }
         }
@@ -57,10 +59,14 @@ int main()
         int ini,fin,peso; 
         cin>>ini>>fin>>peso;
         grafo[ini].push_back(make_pair(peso,fin));
-        grafo[fin].push_back(make_pair(peso,ini));
+        // si es no dirigo descomentar la linea de abajo 
+        // grafo[fin].push_back(make_pair(peso,ini));
     }
-    dijkstra(0);
+    int source, destination; 
+    cin >> source >> destination;
+    dijkstra(source);
     for(int i = 0;i<vertices;i++) {
         cout<<distancia[i]<<endl;
     }
+    cout<< distancia[destination]<<endl;
 }

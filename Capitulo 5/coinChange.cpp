91f9729a16210192393e6340027407c220b000ca
Paul Landaeta <paul.landeata@coxautoinc.com>
2022-05-05 09:15:47 -0400

    feat: coin change TP

--- Capitulo 5/coinChange.cpp
+++ Capitulo 5/coinChange.cpp
@@ -35,14 +35,56 @@ int dpTopDown(int n) {
     } 
     return dp[n];
 }
+
+int coinChangeBottomUP(int value) {
+    int dp2[5][1000];
+    memset(dp2,0,sizeof dp2);
+    for(int i = 1; i <= value ;i++) {
+        dp2[0][i] = 1e9;
+    }
+
+    for (int i = 1 ;i <= 4; i++ ) {
+        for(int j = 0; j <= value ; j++ ){
+            if(j-coins[i-1]>=0) {
+                int valor = 1 + dp2[i][j-coins[i-1]];
+                dp2[i][j] = min (valor, dp2[i-1][j]);
+            }
+            else {
+                dp2[i][j] = dp2[i-1][j];
+            }
+        }
+    }
+    
+    return dp2[4][value];
+}
+
 int main() {
-    int n = 1;
-    memset(dp,-1,sizeof(dp));
+    int n = 100;
+    memset(dp,-1,sizeof(dp)); // -1 0 1 
+    // sizeof(dp) el tamaño del vector
+    // for (int i = 0; i < sizeof(dp); i++) {
+    //     dp[i] = -1;
+    // }
+    // int grafo[100][100];
+    // memset(grafo, 0, sizeof(grafo));
+    // for (int i =0 ;i< 100;i++) {
+    //     for(int j=0;j<100;j++) {
+    //         grafo[i][j] = 0;
+    //     }
+    // }
+    cout<<"Top Down "<<endl;
     int result =  dpTopDown(n);
     if(result< INF){
         cout<<result <<endl;
     }else {
         cout<<"-1"<<endl;
+    }
+
+
+    cout<<"bottom up"<<endl;
+    int result2 = coinChangeBottomUP(n);
+    if(result2 < INF){
+        cout<<result2 <<endl;
     }
         
     return 0;
